680a4794b1e178cb7768a814c2f552827a9099ec
Sam Morrison <sammorrison@192-168-1-2.tpgi.com.au>
2015-11-06 22:49:21 +1100

    chapter 1,2,3 completed

---
+++ client/templates/posts/posts_list.js
@@ -0,0 +1,21 @@
+
+//Dummy data, not using database yet
+var postsData = [
+  {
+    title: 'Introducing Telescope',
+    url: 'http://sachagreif.com/introducing-telescope/'
+  }, 
+  {
+    title: 'Meteor',
+    url: 'http://meteor.com'
+  }, 
+  {
+    title: 'The Meteor Book',
+    url: 'http://themeteorbook.com'
+  }
+];
+
+//helper for postsList
+Template.postsList.helpers({
+  posts: postsData
+});
